1294b41cef0b0fe81c47d1144e6f21b33a50ded1
crimson-lake <sandra.zielinska@protonmail.com>
2019-10-17 21:51:42 +0200

    Injected DAO to controller

--- web-crm/src/pl/zielinska/crmdemo/controller/CustomerController.java
+++ web-crm/src/pl/zielinska/crmdemo/controller/CustomerController.java
@@ -1,16 +1,29 @@
 package pl.zielinska.crmdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import pl.zielinska.crmdemo.dao.CustomerDAO;
+import pl.zielinska.crmdemo.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
+	@Autowired
+	private CustomerDAO customerDAO;
+	
 	@RequestMapping("/list")
 	public String listCustomers(Model theModel) {
+		
+		List<Customer> theCustomers = customerDAO.getCustomers();
+		
+		theModel.addAttribute("customers", theCustomers);
+		
 		return "list-customers";
 	}
-
 }
