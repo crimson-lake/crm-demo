6e4334cebd01c0472f4914fb9c9db6d898bc8490
crimson-lake <sandra.zielinska@protonmail.com>
2019-10-19 22:17:55 +0200

    Added @AfterReturning advice

--- web-crm/src/pl/zielinska/crmdemo/aspect/CRMLoggingAspect.java
+++ web-crm/src/pl/zielinska/crmdemo/aspect/CRMLoggingAspect.java
@@ -3,6 +3,7 @@ package pl.zielinska.crmdemo.aspect;
 import java.util.logging.Logger;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -29,9 +30,24 @@ public class CRMLoggingAspect {
 	
 	@Before("forAppFlow()")
 	public void before(JoinPoint theJoinPoint) {
-		
 		String theMethod = theJoinPoint.getSignature().toShortString();
 		myLogger.info("*** @Before: calling method: " + theMethod + " ***");
 		
+		Object[] args = theJoinPoint.getArgs();
+		
+		for(Object arg : args) {
+			myLogger.info("*** argument: " + arg + " ***");
+		}
 	}
+	
+	@AfterReturning(
+			pointcut="forAppFlow()",
+			returning="theResult")
+	public void afterReturning(JoinPoint theJoinPoint, Object theResult) {
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info("*** @AfterReturning: from method: " + theMethod + " ***");
+		
+		myLogger.info("*** result: " + theResult + " ***");
+	}
+	
 }
