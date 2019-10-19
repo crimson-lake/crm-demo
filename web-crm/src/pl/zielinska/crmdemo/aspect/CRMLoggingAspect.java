c9344e812f6e5ff1443786951925c097c8de8c25
crimson-lake <sandra.zielinska@protonmail.com>
2019-10-19 21:55:39 +0200

    combined pointcut for app flow

--- web-crm/src/pl/zielinska/crmdemo/aspect/CRMLoggingAspect.java
+++ web-crm/src/pl/zielinska/crmdemo/aspect/CRMLoggingAspect.java
@@ -22,6 +22,6 @@ public class CRMLoggingAspect {
 	@Pointcut("execution(* pl.zielinska.crmdemo.dao.*.*.(..))")
 	private void forDAOPackage() {}
 	
-	
-
+	@Pointcut("forControllerPackage || forServicePackage() || forDAOPackage()")
+	private void forAppFlow() {}
 }
